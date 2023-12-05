073922b951588f164e5460e30ac3b5a6291139fc
Yulia Melnyk <yuliia.melnyk@symphony-solutions.com>
2023-12-05 17:58:47 +0100

    [INFRA] Add build workflow2

--- lesson9/my-new-cypress-project/cypress.config.js
+++ lesson9/my-new-cypress-project/cypress.config.js
@@ -22,7 +22,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000'
+    //baseUrl: 'http://localhost:3000'
   },
 });
 
